f5f21c20b13c163395a9572d94b525a799bb4aab
SripaadaBhat <149899794+SripaadaBhat@users.noreply.github.com.>
2026-09-21 22:43:11 +0530

    Menu page done

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+import menu from "src/images/MW17-Flat-Menu-BullFish-1.jpg";
+
+export default function loadMenu(){
+    const content=document.querySelector("#content");
+const menuCard=document.createElement("img");
+menuCard.src=menu;
+menuCard.alt="Menu Card";
+content.append(menuCard);
+}
